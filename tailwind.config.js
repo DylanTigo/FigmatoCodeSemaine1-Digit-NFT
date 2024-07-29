4a3678d98c8436cc1866ce74110545e1c0371de2
dylantigo <dylannoeltigomo@gmail.com>
2024-07-29 22:19:34 +0100

    Configution and assets

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        grey: "#F8F8F8",
+        neutral: {
+          50: "#E8E9EA",
+          100: "#B8BABE",
+          200: "#96989E",
+          300: "#666A72",
+          400: "#484D56",
+          500: "#1A202C",
+          600: "#181D28",
+          700: "#12171F",
+          800: "#0E1218",
+          900: "#0B0D12",
+        },
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+      spacing: {
+        section: "24rem",
+      },
+    },
+  },
+  plugins: [],
+};
